7a6ff700402057adc15e5c96c2e2a01a255f6591
Edilson Tarcio <tarcio.ecp@gmail.com.br>
2023-04-16 21:11:35 -0300

    Adicionando o AdminLTE3

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        Sistema Escolar
+    </div>
+    <!-- Default to the left -->
+    <strong>Direitos autorais &copy; 2023-<?= date('Y') ?> <a href="#">Upfuture</a>.</strong> Todos os direitos reservados.
+</footer>
